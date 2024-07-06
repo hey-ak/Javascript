9dff03cfb8346f9e0b6e79ec37b5f66f5406c309
hey-ak <akshay0127.be21@chitkara.edu.in>
2024-07-06 11:37:26 +0530

    added variables

---
+++ Basics/variables.js
@@ -0,0 +1,16 @@
+// console.log("Hello")
+
+const accountId = 123456
+let accountMail = "akshay@gmail.com"
+var accountPassword = "12345" // prefer not to use var 
+let userState;
+
+accountCity = "Rajpura"
+
+// accountId = 34 Not allowed
+
+accountMail = "hi@gmail.com"
+accountPassword = "987546"
+accountCity = "Bengaluru"
+
+console.table([accountMail,accountPassword,accountCity,userState])
