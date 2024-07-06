b24a5614a6ccfd4152e2cb82c0055dff37cbefdc
hey-ak <akshay0127.be21@chitkara.edu.in>
2024-07-06 16:09:45 +0530

    datatypes added with notes

---
+++ Basics/datatypes.js
@@ -0,0 +1,21 @@
+"use strict" ; // treat all code as new version of js
+// alert() // we are using node js not browser so we can't use it
+
+
+let name = "Akshay"
+let age = 25
+let isLoggedIn = true
+// Primative data types
+//--------------------------------------------------------
+// number - 2 to the power 53
+// bigint
+// string - ""
+// boolean - true/ false
+// null - stand alone value
+// undefined - value not assigned yet
+// symbol - unique
+
+// object
+
+console.log(typeof undefined) // value is undefined 
+console.log(typeof null); // value is object
