77e829f8a5d8583c3f7a5e8f771d99c3c9319e59
hey-ak <akshay0127.be21@chitkara.edu.in>
2024-07-06 16:29:20 +0530

    conversion of data types with notes added

---
+++ Basics/conversionOperation.js
@@ -0,0 +1,20 @@
+let score = "77abc"
+// console.log(typeof(score));
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber);
+
+// "33"  => 33
+//  "33abc" => NaN
+// true => 1 ; false => 0
+
+
+let isLoggedIn = 6
+// console.log(Boolean(isLoggedIn));
+// 1 - true ; 0 - false
+// "" - false; "somevalue" - true
+
+
+let someNumber = 127
+let someString = String(someNumber);
+console.log(typeof someString);
