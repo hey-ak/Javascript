ad524a33afc6ec9d9b9fcc91d84ef29f83411ec1
hey-ak <akshay0127.be21@chitkara.edu.in>
2024-07-06 20:08:06 +0530

    Added Strings

---
+++ Basics/strings.js
@@ -0,0 +1,27 @@
+const name = "akshay"
+const repoCount = 71
+// console.log(name+repoCount+"Jha");
+
+// console.log(`Hi My Name is ${name} and my repocount id ${repoCount}`);
+const Newname =  new String("Akshay")
+// console.log(Newname[0]);
+// console.log(Newname.__proto__);
+// console.log(Newname.length);
+// console.log(Newname.toUpperCase())
+// console.log(Newname.charAt(4));
+// console.log(Newname.indexOf('h'));
+const substrName = Newname.substring(0,4)
+// console.log(slicedName);
+
+const slicedName = Newname.slice(-8,4)
+// console.log(slicedName);
+
+const MyName = "     Shubham     "
+console.log(MyName);
+console.log(MyName.trim());
+
+const url = "https://akshay-ios-programmer.com" 
+// console.log(url.replace("akshay","shubham"));
+// console.log(url.includes("akshay"));
+console.log(url.split('-'));
+
